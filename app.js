1bff0ac47ee9a5a1ee578ff565a30326f02c05ad
SuperScript <superscript18@gmail.com>
2016-04-02 07:59:29 -0600

    a

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.view = new AppView ({
 app.messages = {
     'making':  [ 'STARTING WORKTABLE', 'We\'re starting up your group', 'Contacting the room server...' ],
     'joining': [ 'JOINING WORKTABLE', 'Connecting you with your group', 'Contacting peers...' ],
-    'authing': [ 'GETTING PERMISSION', 'Your group will be including you soon', 'Waiting for response...' ],
+    'authing': [ 'GETTING PERMISSION', 'Your\'ll be in soon', 'Waiting for response...' ],
     '...': [ 'CONNECTING', 'YOUR SESSION MIGHT BE LOADING...', 'Stuff is happening' ],
 }
 // start working
